05947966fdf6ede43644ca3b2ef817a1e0c62059
Marisol Smith <marisolsmith@MacBook-Air.local>
2025-06-01 20:26:00 -0400

    modified vite.config and added /dist folder to main

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,15 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/json-view-for-notion/',
+  base: "/json-view-for-notion/",
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
@@ -21,12 +19,13 @@ export default defineConfig(({ mode }) => ({
     },
   },
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
+    outDir: "dist",
+    assetsDir: "assets",
     rollupOptions: {
       output: {
         manualChunks: undefined,
       },
     },
+    emptyOutDir: true, // ensures clean build
   },
 }));
